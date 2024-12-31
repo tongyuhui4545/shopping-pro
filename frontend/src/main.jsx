af0a996d055409f635c8ab9be89f2ce74e6b78bb
Sam <tongyuhui@gmail.com>
2025-01-01 00:24:28 +1100

    feat: forntend construct

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+import './assets/styles/index.css'
+import './assets/styles/bootstrap.custom.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen.jsx'
+import ProductScreen from './screens/ProductScreen.jsx'
 import App from './App.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route  path="/product/:id" element={<ProductScreen />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
