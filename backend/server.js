65c825b5abb8eebaefc7945b4ad198c1e80c9d0c
Sam <tongyuhui@gmail.com>
2025-01-01 23:37:48 +1100

    chore: data modeling

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js'
+
+dotenv.config();
+connectDB();
 
 const port = process.env.PORT || 5000;
 
