c34ae7629d4065fd7d1e37c1f8b052444d919451
Sam <tongyuhui@gmail.com>
2025-01-03 14:55:04 +1100

    chore: new branch dev

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,4 @@
 import express from "express"
-// import asyncHandler from "../middleware/asyncHandler.js";
-// import Product from "../models/productModel.js";
 import {getProducts, getProductById} from '../controllers/productControllers.js'
 
 const router = express.Router();
