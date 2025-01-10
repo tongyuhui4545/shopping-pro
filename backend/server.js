99a7955ded81472a32f987703e163b29d2d21ea3
Sam <tongyuhui@gmail.com>
2025-01-10 21:34:30 +1100

    chore: auth and update user && auth middleware

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js'
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config();
 connectDB();
@@ -11,11 +13,18 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send('yolo.....');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
