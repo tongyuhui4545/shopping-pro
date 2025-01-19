1a675fd06854610800c232608ca56f8c9d6b3100
Sam <tongyuhui@gmail.com>
2025-01-19 19:15:17 +1100

    feat: finished project, ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -19,26 +19,28 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 //Cookie parser middleware
 app.use(cookieParser());
-
-app.get('/', (req, res) => {
-    res.send('yolo.....');
-});
-
-const __dirname = path.resolve(); 
-
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
-
 app.get('/api/config/paypal', (req, res) => res.send({
     clientId: process.env.PAYPAL_CLIENT_ID
 }))
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('yolo.....');
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
